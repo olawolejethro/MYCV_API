9d31ae971aba6f8c82271ec84932a0cd74f66e32
olawolejethro <kunlekzi2@gmail.com>
2024-08-09 12:19:33 +0100

    db_setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { UsersModule } from 'src/users/users.module';
 import { ReportsModule } from 'src/reports/reports.module';
+import { User } from 'src/users/user.entity';
+import { Report } from 'src/reports/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User, Report],
       synchronize: true,
     }),
     UsersModule,
---
+++ src/users/user.entity.ts
@@ -0,0 +1,10 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column()
+  Password: string;
+}
