d1d6737b5a8d0d1cbe7795d98a17eb1c853daba6
olawolejethro <kunlekzi2@gmail.com>
2024-08-09 03:12:50 +0100

    create_entity

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { UsersModule } from 'src/users/users.module';
+import { ReportsModule } from 'src/reports/reports.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
+  providers: [AppService],
+  controllers: [AppController],
+})
+export class AppModule {}
