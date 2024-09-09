c5615ad53c3320bac6f8835b796566290b6ba509
olawolejethro <kunlekzi2@gmail.com>
2024-09-09 03:04:19 +0100

    fit/Association

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,13 +2,17 @@ import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report-Dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
-
+import { CurrentUser } from 'src/users/decorators/currrent-user.decorator';
+import { User } from 'src/users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 @Controller('reports')
 export class ReportsController {
   constructor(private reporsService: ReportsService) {}
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reporsService.create(body);
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reporsService.create(body, user);
   }
 }
